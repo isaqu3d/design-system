414bcbd186fd23db9424a4f77a2cb86d209d6307
isaque.s <isaque.error404@gmail.com>
2022-10-13 13:04:10 -0300

    feat: add fontSize and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+
+    colors: {
+      transparent: "transparent",
+
+      black: "#000",
+      white: "#fff",
+
+      gray: {
+        900: "#111214",
+        800: "#202024",
+        400: "#7c7c8a",
+        200: "#c4c4cc",
+        100: "#e1e1e6",
+      },
+
+      cyan: {
+        500: "#81d8f7",
+        300: "#9ad8ee",
+      },
+    },
     extend: {
       fontFamily: {
         sans: "Inter, sans-serif",
