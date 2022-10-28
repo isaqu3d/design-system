0e64e0d444b6198ac83b58c1b080d638d25cf616
isaque.s <isaque.error404@gmail.com>
2022-10-28 10:06:36 -0300

    feat: create pages and create signin stories

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,43 @@
+import { expect } from "@storybook/jest";
+import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { rest } from "msw";
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Pages/Sign in",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "isaque.error404@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("*********"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
